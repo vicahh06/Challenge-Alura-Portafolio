c543f5712ae1e7b6ed8d570ee8ebd991a669c35b
vicahh06 <vahh03@gmail.com>
2024-04-30 00:29:37 -0600

    Validacion JS

--- validacion.js
+++ validacion.js
@@ -1,1 +1,23 @@
 //Haz tú validación en javascript acá
+function validarDatos(){
+    window.event.preventDefault();
+
+    if(document.form.nombre.value == "") {
+        alert("Campo nombre obligatorio");
+        document.form.nombre.focus();
+    }else if (document.form.email.value == "") {
+        alert("Campo E-mail obligatorio");
+        document.form.email.focus();
+    } else if (document.form.asunto.value == "") {
+        alert("Campo asunto obligatorio");
+        document.form.asunto.focus();
+    } else if (document.form.email.value.indexOf("@") == -1 || document.form.email.value.indexOf(".") == -1) {
+       alert("email es invalido");
+       document.form.email.focus();
+    } else if (document.form.mensaje.value == "" || document.form.mensaje.length <=50) {
+        alert("campo mensaje obligatorio y debe tener maximo 50 caracteres");
+        document.form.mensaje.focus();
+    }
+}
+
+document.querySelector("form").addEventListener("submit", validarDatos);
